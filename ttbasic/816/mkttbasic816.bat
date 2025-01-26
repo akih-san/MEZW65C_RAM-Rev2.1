4ee62f9a74bf17b72da7972c48134469d4eaf262
@Akih_san <112370246+akih-san@users.noreply.github.com>
2025-01-26 22:09:27 +0900

    Delete ttbasic/816 directory

--- ttbasic/816/mkttbasic816.bat
+++
@@ -1,7 +0,0 @@
-rem WDC816CC ttbasic.c -A
-WDC816AS -G -L ttbasic816.asm -o aaa.obj
-WDCLN -HB -g -t aaa.obj -o aaa.bin
-bin2mot -L24F0 -IC400 aaa.bin aaa.s
-mot2bin aaa.s TTBASIC.B16
-copy TTBASIC.B16 ..\..\DISKS\TTBASIC\.
-del aaa.*
