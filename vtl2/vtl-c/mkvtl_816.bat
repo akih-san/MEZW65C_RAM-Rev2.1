a3fc0621e5319b6987cc86f1338090382e496f0a
@Akih_san <112370246+akih-san@users.noreply.github.com>
2025-01-26 22:12:09 +0900

    Delete vtl2/vtl-c directory

--- vtl2/vtl-c/mkvtl_816.bat
+++
@@ -1,7 +0,0 @@
-rem WDC816CC vtl-c.c -A
-WDC816AS -G -L vtl2_816.asm -o aaa.obj
-WDCLN -HB -g -t aaa.obj -o aaa.bin
-bin2mot -L0A00 -IDF00 aaa.bin aaa.s
-mot2bin aaa.s VTL2.B16
-copy VTL2.B16 ..\..\DISKS\VTL2\.
-del aaa.*
