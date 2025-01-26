30a7cbd7cab5ce1d6e43b3380abad87608e0d95d
@Akih_san <112370246+akih-san@users.noreply.github.com>
2025-01-26 22:11:05 +0900

    Delete GAME-C/816 directory

--- GAME-C/816/mkgame816.bat
+++
@@ -1,6 +0,0 @@
-WDC816AS -G -L game-c816.asm -o aaa.obj
-WDCLN -HB -g -t aaa.obj -o aaa.bin
-bin2mot -L1780 -ID100 aaa.bin aaa.s
-mot2bin aaa.s GAME-C.B16
-copy GAME-C.B16 ..\..\DISKS\GAME-C\.
-del aaa.*
